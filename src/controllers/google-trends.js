c80edc64df2a6d0355d50b019a8b53af9aec615a
dougrosa0 <d.rosa5@yahoo.com>
2024-12-02 15:20:44 -0700

    upgrade aws sdk to v3

--- src/controllers/google-trends.js
+++ src/controllers/google-trends.js
@@ -1,13 +1,15 @@
-const aws = require('aws-sdk');
+const AWS_REGION = 'us-west-2';
+
+const { DynamoDBClient } = require("@aws-sdk/client-dynamodb");
+const { DynamoDBDocumentClient, QueryCommand, PutCommand } = require("@aws-sdk/lib-dynamodb");
+const { SESClient, SendEmailCommand } = require("@aws-sdk/client-ses");
 const googleTrends = require('google-trends-api');
 
-aws.config.update({
-  region: 'us-west-2'
-});
-const docClient = new aws.DynamoDB.DocumentClient();
+const client = new DynamoDBClient({ region: AWS_REGION });
+const docClient = DynamoDBDocumentClient.from(client);
+const sesClient = new SESClient({ region: AWS_REGION });
 
 exports.emailTrends = async function(req) {
-  console.log(req.body);
   var searchDate = req.body.date;
   var email = req.body.email
   console.log("Emailing trends for " + searchDate);
@@ -23,67 +25,55 @@ exports.emailTrends = async function(req) {
       }
   };
 
-  docClient.query(params, function(err, data) {
-      if (err) {
-          console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-      } else {
-          var emailContent = "<h2>Trending Google Searches " + searchDate + "</h2>";
-          emailContent += "<table><tr><th>Rank</th><th>Search</th><th>Traffic amount</th></tr>";
-          console.log("Query succeeded.");
-          console.log("Query text | Day rank | Traffic amount | Query link")
-          data.Items.sort(function(a, b) {
-              return a.dayRank - b.dayRank;
-          })
-          data.Items.forEach(function(item) {
-              emailContent += "<tr><td>" + item.dayRank + "</td><td><a href=\"" + encodeURI('https://google.com/search?q=' + item.queryText) + "\">" + item.queryText + "</a></td><td>" + item.trafficAmount + "</td></tr>";
-              console.log(item.queryText + " | " + item.dayRank + " | " + item.trafficAmount + " | " + item.queryLink);
-          });
-          emailContent += "</table>";
-          sendEmail(emailContent);
-      }
-  });
+  try {
+    const result = await docClient.send(new QueryCommand(params));
+    let emailContent = "<h2>Trending Google Searches " + searchDate + "</h2>";
+    emailContent += "<table><tr><th>Rank</th><th>Search</th><th>Traffic amount</th></tr>";
+    
+    result.Items.sort((a, b) => a.dayRank - b.dayRank);
+    
+    result.Items.forEach(function(item) {
+      emailContent += "<tr><td>" + item.dayRank + "</td><td><a href=\"" + encodeURI('https://google.com/search?q=' + item.queryText) + "\">" + item.queryText + "</a></td><td>" + item.trafficAmount + "</td></tr>";
+    });
+    
+    emailContent += "</table>";
+    await sendEmail(email, emailContent);
+  } catch (err) {
+    console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
+  }
+};
 
-  function sendEmail(content) {
-
-    // Create sendEmail params data
-    var params = {
-      Destination: {
-        ToAddresses: [
-          email
-        ]
-      },
-      Message: {
-        Body: {
-          Html: {
+async function sendEmail(email,content) {
+  const params = {
+    Destination: {
+      ToAddresses: [email]
+    },
+    Message: {
+      Body: {
+        Html: {
           Charset: "UTF-8",
           Data: content
           },
           Text: {
           Charset: "UTF-8",
           Data: content
-          }
-        },
-        Subject: {
-          Charset: 'UTF-8',
-          Data: 'Google Trends'
         }
-        },
-      Source: 'dougrosa0@gmail.com'
-    };
+      },
+      Subject: {
+        Charset: 'UTF-8',
+        Data: 'Google Trends'
+      }
+    },
+    Source: 'dougrosa0@gmail.com'
+  };
 
-    // Create the promise and SES service object
-    var sendPromise = new aws.SES({apiVersion: '2010-12-01'}).sendEmail(params).promise();
-
-    // Handle promise's fulfilled/rejected states
-    sendPromise.then(
-      function(data) {
-        console.log("Email sent successfully. MessageId: " + data.MessageId);
-      }).catch(
-        function(err) {
-        console.error(err, err.stack);
-      });
+  try {
+    const result = await sesClient.send(new SendEmailCommand(params));
+    console.log("Email sent successfully. MessageId: " + result.MessageId);
+  } catch (err) {
+    console.error(err, err.stack);
   }
-};
+}
 
 exports.readTrends = async function(req) {
   var searchDate = req.params.date;
@@ -99,42 +89,18 @@ exports.readTrends = async function(req) {
     }
   };
 
-  try {
-    const data = await new Promise((resolve, reject) => {
-      docClient.query(params, function (err, data) {
-        if (err) {
-          console.error("Unable to query. Error:", JSON.stringify(err, null, 2));
-          reject(err);
-        } else {
-          data.Items.sort(function (a, b) {
-            return a.dayRank - b.dayRank;
-          })
-          const trends = data.Items.map(function (item) {
-            return {
-              dayRank: item.dayRank,
-              queryText: item.queryText,
-              trafficAmount: item.trafficAmount
-            };
-          });
-          resolve(trends);
-        }
-      });
-    });
+  const result = await docClient.send(new QueryCommand(params));
+  const trends = result.Items.sort((a, b) => a.dayRank - b.dayRank)
+    .map(item => ({
+      dayRank: item.dayRank,
+      queryText: item.queryText,
+      trafficAmount: item.trafficAmount
+    }));
 
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(data),
-    };
-
-    return response;
-  } catch (err) {
-    console.error("Error:", JSON.stringify(err, null, 2));
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ message: "Internal Server Error" }),
-    };
-  }
-
+  return {
+    statusCode: 200,
+    body: JSON.stringify(trends),
+  };
 };
 
 exports.writeTrends = async function() {
@@ -147,19 +113,20 @@ exports.writeTrends = async function() {
       const day = days[i];
       const trendingSearches = day.trendingSearches;
       const searchDate = day.date;
+      console.log("Writing trends for " + searchDate);
 
       for (let j = 0; j < trendingSearches.length; j++) {
         const search = trendingSearches[j];
         const rank = j+1;
         const query = search.title.query;
         const trafficAmount = search.formattedTraffic;
-        console.log(searchDate + " " + query + " " + trafficAmount);
         await saveItem(query, searchDate, trafficAmount, rank);
       }
     }
+    console.log("Trends saved successfully");
     return {
       statusCode: 200,
-      body: JSON.stringify({ message: "Items saved to DynamoDB" })
+      body: JSON.stringify({ message: "Trends saved" })
     };
   } catch (err) {
     console.error(err);
@@ -170,9 +137,7 @@ exports.writeTrends = async function() {
   }
 };
 
-
-
-function saveItem(queryString, searchDate, trafficAmount, dayRank) {
+async function saveItem(queryString, searchDate, trafficAmount, dayRank) {
   const queryLink = encodeURI('https://google.com/search?q=' + queryString);
   const params = {
     Item: {
@@ -184,5 +149,5 @@ function saveItem(queryString, searchDate, trafficAmount, dayRank) {
     },
     TableName: "googleTrends"
   };
-  return docClient.put(params).promise();
+  return docClient.send(new PutCommand(params));
 }
