032afae38134fe6b67192b0ca95b849caed27aca
dougrosa0 <d.rosa5@yahoo.com>
2024-12-04 20:10:28 -0700

    support heroku random port assignment

--- src/api-server.js
+++ src/api-server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const trendsRoutes = require('./routes/trends-routes');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
